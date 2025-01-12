41d81c8b277821b493ed03ec1b2b826d0b7e943a
ppdfr84 <pedro.rocha@csjsistemas.com.br>
2025-01-12 20:34:20 -0300

    Mostrar Swagger em todos os ambientes

--- medscanner-api/Program.cs
+++ medscanner-api/Program.cs
@@ -74,13 +74,9 @@ builder.Services.AddScoped<UsuariosService>();
 
 var app = builder.Build();
 
-
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
